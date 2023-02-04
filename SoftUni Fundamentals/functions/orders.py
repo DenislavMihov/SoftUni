8a8eaafb47be452fca4fa3ffdf9412a6e8dcb6fe
DenislavMihov <d.i.mihov@gmail.com>
2023-02-04 11:15:04 +0200

    Update orders.py

--- SoftUni Fundamentals/functions/orders.py
+++ SoftUni Fundamentals/functions/orders.py
@@ -1,6 +1,7 @@
 def price_of_order(product, quantity):
 
     result = 0
+
     if product == "coffee":
         result = 1.50 * quantity
     if product == "water":
@@ -9,6 +10,7 @@ def price_of_order(product, quantity):
         result = 1.40 * quantity
     if product == "snacks":
         result = 2.00 * quantity
+
     return result
 
 product = input()
