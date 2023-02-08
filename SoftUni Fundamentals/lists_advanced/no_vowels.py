691eb0169b2df4f1a3b5b4b22cc6d80e287e9a41
DenislavMihov <d.i.mihov@gmail.com>
2023-02-08 20:04:44 +0200

    list advanced tasks

---
+++ SoftUni Fundamentals/lists_advanced/no_vowels.py
@@ -0,0 +1,10 @@
+text = input()
+result = [char for char in text if char.lower() not in ['a', 'o', 'u', 'e', 'i']]
+
+
+#for char in text:
+#    # make case insensitive
+#    if char.lower() not in ['a', 'o', 'u', 'e', 'i']:
+#       result.append(char)
+
+print("".join(result))
