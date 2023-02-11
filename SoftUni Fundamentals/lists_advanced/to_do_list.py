41208532782987c8d1354fe617f0a7a3c91479a0
DenislavMihov <d.i.mihov@gmail.com>
2023-02-11 11:51:31 +0200

    Update to_do_list.py

--- SoftUni Fundamentals/lists_advanced/to_do_list.py
+++ SoftUni Fundamentals/lists_advanced/to_do_list.py
@@ -1,10 +1,10 @@
-to_do_list = [] * 10
+to_do_list = [0] * 10
 
 command = input()
 
 while command != "End":
     importance, task = command.split("-")
-    importance = int()
+    importance = int(importance)
     index = importance - 1
     to_do_list[index] = task
     command = input()
