c5d37d66b387e5e1f39275f6a307844107e5dee2
DenislavMihov <d.i.mihov@gmail.com>
2023-03-11 09:25:21 +0200

    Create emoticon_finder.py

---
+++ SoftUni Fundamentals/text_processing_exercise/emoticon_finder.py
@@ -0,0 +1,8 @@
+text = input()
+parts = text.split(":")
+
+for idx in range(len(text)):
+    ch = text[idx]
+    if ch == ":" and idx + 1 < len(text):
+        symbol = text[idx + 1]
+        print(f":{symbol}")
