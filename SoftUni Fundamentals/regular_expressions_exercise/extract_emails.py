cd2f96ab84a59cddb8a8094d90434cfef96565c0
DenislavMihov <d.i.mihov@gmail.com>
2023-03-22 19:54:06 +0200

    Update extract_emails.py

--- SoftUni Fundamentals/regular_expressions_exercise/extract_emails.py
+++ SoftUni Fundamentals/regular_expressions_exercise/extract_emails.py
@@ -1,7 +1,6 @@
 import re
 
 text = input()
-#pattern = r"\b[A-Za-z0-9][\w\-.]*[A-Za-z0-9]@[A-Za-z][\w\-.]*[A-Za-z]"
 pattern = r"\s([A-Za-z0-9][\w\-.]*[A-Za-z0-9]@[A-Za-z][A-Za-z\-]*[A-Za-z](\.[A-Za-z][A-Za-z\-]*[A-Za-z])+)"
 
 emails = re.findall(pattern, text)
